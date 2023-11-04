1d411068ea6f18c472c9cf883e42d20dbd32ad6c
Nikita <erofeev.nik.it@yandex.ru>
2023-11-05 01:01:27 +0300

    Initial commit

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,7 @@
+
+7
+main.py,f/1/f1bdda93d9a278e358509d498e17d97764c1fb29
+>
+pyproject.toml,5/d/5d07e7d72637aa0d59c89d381fe6dc4cf46e2491
+h
+8venv/lib/python3.11/site-packages/starlette/responses.py,7/0/705ba0639cac19653af5adb7a9599fc1774d1729
---
+++ main.py
@@ -0,0 +1,26 @@
+import qrcode
+from fastapi import FastAPI
+from starlette.responses import StreamingResponse
+from io import BytesIO  # для сохр в памяти
+import uvicorn
+
+app = FastAPI()
+
+
+@app.get('/generate-qr-code')
+def generate_qrcode(string: str) -> StreamingResponse:
+    qr = qrcode.QRCode(box_size=10,
+                       border=10)
+
+    qr.add_data(string)
+
+    image = qr.make_image()
+
+    data = BytesIO()
+    image.save(data)
+
+    data.seek(0)
+    return StreamingResponse(data, media_type='img/png', headers={"Content-Disposition": "attachment; filename=generate-qr-code.png"})
+
+
+uvicorn.run(app, port=9500)
